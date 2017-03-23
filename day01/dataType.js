b21c3b5f7e2b834d915fd4f0c2319c75da1ea792
YuEcho <yhyecho@gmail.com>
2017-03-23 14:52:03 +0800

    add Number dataType

---
+++ day01/dataType.js
@@ -0,0 +1,8 @@
+// js 数据类型
+// [1] Number
+const int = 123 // 整数
+const float = 0.456 // 浮点数
+const double = 1.2345e3 // 科学计数法表示 1.2345*1000
+const int2 = -99 // 负数
+// NaN Not a Number, 当无法计算结果时用NaN表示
+// Infinity 超过js最大值时用此表示
