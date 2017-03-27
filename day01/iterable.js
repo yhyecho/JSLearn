8379e9a2858cba1c538a3e891581f2c5c78ca9ab
YuEcho <yhyecho@gmail.com>
2017-03-27 09:45:15 +0800

    add iterable

--- day01/iterable.js
+++ day01/iterable.js
@@ -34,3 +34,28 @@ for (var x of a) {
 }
 
 // 然而，更好的方式是直接使用iterable内置的forEach方法，它接收一个函数，每次迭代就自动回调该函数。以Array为例：
+var a = ['A', 'B', 'C'];
+a.forEach(function (element, index, array) {
+    // element: 指向当前元素的值
+    // index: 指向当前索引
+    // array: 指向Array对象本身
+});
+
+// Set与Array类似，但Set没有索引，因此回调函数的前两个参数都是元素本身：
+var s = new Set(['A', 'B', 'C']);
+s.forEach(function (element, sameElement, set) {
+    alert(element);
+});
+
+// Map的回调函数参数依次为value、key和map本身：
+var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
+m.forEach(function (value, key, map) {
+    alert(value);
+});
+
+// 如果对某些参数不感兴趣，由于JavaScript的函数调用不要求参数必须一致，
+// 因此可以忽略它们。例如，只需要获得Array的element：
+var a = ['A', 'B', 'C'];
+a.forEach(function (element) {
+    alert(element);
+});
